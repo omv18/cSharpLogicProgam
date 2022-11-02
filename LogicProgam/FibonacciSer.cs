2407446f8e53a7da25260328c3808c82f20e19b7
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-02 08:43:47 +0530

    [sourav]add : fiboSeries

---
+++ LogicProgam/FibonacciSer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProgam
+{
+    internal class FibonacciSer
+    {
+        public void fiboSer(int num)
+        {
+            int a = 0 ,b = 1;
+            Console.Write("Fibonacci Series is : "+ a + " " + b);
+            for(int i =0; i < num; i++)
+            {
+                int c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+            
+        }
+    }
+}
---
+++ LogicProgam/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LogicProgam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSer fibSer = new FibonacciSer();
+            fibSer.fiboSer(9);
+
+        }
+    }
+
+}
+
