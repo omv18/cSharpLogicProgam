45657e5741687e3bd59c8f774e7ec64d99da29b4
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-02 09:01:36 +0530

    [sourav} add : PrefectDone

---
+++ LogicProgam/PerfectNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProgam
+{
+    internal class PerfectNum
+    {
+        int sum = 0;
+        public bool perfectCheck(int num)
+        {
+            
+            for(int i = 1; i <= num/2; i++)
+            {
+                if(num%i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if(sum == num)
+                return true;
+            return false;
+        }
+    }
+}
--- LogicProgam/Program.cs
+++ LogicProgam/Program.cs
@@ -7,6 +7,12 @@ namespace LogicProgam
         {
             FibonacciSer fibSer = new FibonacciSer();
             fibSer.fiboSer(9);
+            PerfectNum armStrong = new PerfectNum();
+            if (armStrong.perfectCheck(28))
+                Console.WriteLine("\n Given Number is Perfect Number");
+            else
+                Console.WriteLine("\n Given Number is not a Perfect Number");
+
 
         }
     }
