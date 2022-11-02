fcb0bb07afce80cf27678b83fbd41b99b5af10b0
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-02 09:35:46 +0530

    [sourav] add : reverseNum

--- LogicProgam/Program.cs
+++ LogicProgam/Program.cs
@@ -5,19 +5,30 @@ namespace LogicProgam
     {
         static void Main(string[] args)
         {
-            FibonacciSer fibSer = new FibonacciSer();
-            fibSer.fiboSer(9);
-            PerfectNum armStrong = new PerfectNum();
-            if (armStrong.perfectCheck(28))
-                Console.WriteLine("\n Given Number is Perfect Number");
-            else
-                Console.WriteLine("\n Given Number is not a Perfect Number");
+            int check = 4;
+            switch (check)
+            {
+                case 1:
+                    FibonacciSer fibSer = new FibonacciSer();
+                    fibSer.fiboSer(9);
+                    break;
+                case 2:
+                    PerfectNum armStrong = new PerfectNum();
+                    if (armStrong.perfectCheck(28))
+                        Console.WriteLine("\n Given Number is Perfect Number");
+                    else
+                        Console.WriteLine("\n Given Number is not a Perfect Number");
+                    break;
+                case 3:
+                    PrimeEven primeEven = new PrimeEven();
+                    primeEven.checkPrime(12222);
+                    break;
+                case 4:
+                    ReverseNum rv = new ReverseNum();
+                    rv.reverse(122);
+                    break;
 
-            //UC3
-            PrimeEven primeEven = new PrimeEven();
-            primeEven.checkPrime(12222);
-
-
+            }
         }
     }
 
---
+++ LogicProgam/ReverseNum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProgam
+{
+    internal class ReverseNum
+    {
+        public void reverse(int num)
+        {
+            int reverse =0; 
+            Console.WriteLine("Number before Reverse is : " + num);
+            while(num > 0)
+            {
+                int remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                
+                num /= 10;
+            }
+            Console.WriteLine("After reverse number is :" + reverse);
+        }
+    }
+}
