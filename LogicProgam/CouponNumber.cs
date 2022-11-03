648c04e18e67bd6da987109286ef83571260c29e
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-03 16:13:53 +0530

    [sourav] add : coupon Number

---
+++ LogicProgam/CouponNumber.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProgam
+{
+    internal class CouponNumber
+    {
+    /*    Random random = new Random();
+        int[] arr = new int[100];
+        bool flag = true;
+        public int generateCoupon(int num)
+        {
+            int coupNum = random.Next(1, num*1000);
+            return coupNum;
+        }
+        public bool checkDistinct(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = i + 1; j < num; j++)
+                {
+                    //Console.WriteLine("I :{0} " + arr[i] + "  J {1}:" + arr[j] + " ", i, j);
+                    int a = arr[i];
+                    int b = arr[j];
+                    int z = a.CompareTo(b);
+                    if (z == 0)
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            return flag;
+        }
+        public void coupon(int num)
+        {
+            for(int i = 0; i < num; i++)
+            {
+                int coupNum;
+                coupNum = generateCoupon(num);
+                arr[i] = coupNum;
+                //Console.Write(arr[i]+"  ");   
+            }
+            bool checkDis = checkDistinct(num);
+            if (flag) 
+                Console.WriteLine("Coupons are the distinct.");
+            else
+                Console.WriteLine("Coupons are the NOT distinct.");
+        }*/
+
+        public static int GenerateRandon()
+        {
+            Random rand = new Random();
+            int randomNum = rand.Next(0, 11);
+            return randomNum;
+        }
+
+        public static void checkCoupon(int num)
+        {
+            int[] arr = new int[num];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int count=0;
+                int randonCoupon = GenerateRandon();
+                for(int j = 0; j <= i; j++)
+                {
+                    if (arr[j] == randonCoupon) 
+                        count++;
+                }
+                if (count == 0) 
+                    arr[i] = randonCoupon;
+            }
+
+            foreach(var data in arr)
+            {
+                if(data != 0)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
--- LogicProgam/Program.cs
+++ LogicProgam/Program.cs
@@ -5,30 +5,40 @@ namespace LogicProgam
     {
         static void Main(string[] args)
         {
-            int check = 4;
-            switch (check)
-            {
-                case 1:
-                    FibonacciSer fibSer = new FibonacciSer();
-                    fibSer.fiboSer(9);
-                    break;
-                case 2:
-                    PerfectNum armStrong = new PerfectNum();
-                    if (armStrong.perfectCheck(28))
-                        Console.WriteLine("\n Given Number is Perfect Number");
-                    else
-                        Console.WriteLine("\n Given Number is not a Perfect Number");
-                    break;
-                case 3:
-                    PrimeEven primeEven = new PrimeEven();
-                    primeEven.checkPrime(12222);
-                    break;
-                case 4:
-                    ReverseNum rv = new ReverseNum();
-                    rv.reverse(122);
-                    break;
-
-            }
+            //bool flag = true;
+            Console.WriteLine("1.Fabonacci Series \n 2.Perfect Number \n 3.Prime Even \n 4.Reverse Number \n 5.Coupon Number \n 6.Exit");
+            int check = Convert.ToInt32(Console.ReadLine());
+            //while (flag)
+            //{
+                switch (check)
+                {
+                    case 1:
+                        FibonacciSer fibSer = new FibonacciSer();
+                        fibSer.fiboSer(9);
+                        break;
+                    case 2:
+                        PerfectNum armStrong = new PerfectNum();
+                        if (armStrong.perfectCheck(28))
+                            Console.WriteLine("\n Given Number is Perfect Number");
+                        else
+                            Console.WriteLine("\n Given Number is not a Perfect Number");
+                        break;
+                    case 3:
+                        PrimeEven primeEven = new PrimeEven();
+                        primeEven.checkPrime(12222);
+                        break;
+                    case 4:
+                        ReverseNum rv = new ReverseNum();
+                        rv.reverse(122);
+                        break;
+                    case 5:
+                        CouponNumber.checkCoupon(9);
+                        //couponNumber.checkCoupon(9);
+                        break;
+                    default:
+                        break;
+                }
+            //}
         }
     }
 
